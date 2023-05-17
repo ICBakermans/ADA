8f1762018edd88ce810481ec498c775dd3cef1b6
levyvankempen <79143661+levyvankempen@users.noreply.github.com>
2023-05-17 23:33:03 +0200

    Update docker_mm.sh

--- installation_script/docker_mm.sh
+++ installation_script/docker_mm.sh
@@ -1,20 +1,13 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2709
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+sudo apt-get update
 
-\f0\fs24 \cf0 sudo apt-get update\
-\
-sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release\
-\
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg\
-\
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null\
-\
-sudo apt-get update\
-\
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose\
-\
-sudo docker run hello-world}
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
+
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg\=
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
+
+sudo docker run hello-world
